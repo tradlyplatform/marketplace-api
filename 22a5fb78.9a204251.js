4f4ace5f47a41a6eacc566e56b4d3944e37f58b1
JK Baseer <jk@istrata.co>
2020-09-15 20:40:16 +0530

    Deploy website - based on a98f1e7870549ad9f50a764da42e365dd72a1932

---
+++ 22a5fb78.9a204251.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{120:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"updates","name":"updates","count":2,"permalink":"/blog/tags/updates"}')}}]);
